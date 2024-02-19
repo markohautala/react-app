88ea6282bcb96c838e961e4e1b4ad309f24bfe7d
markohautala <markohautala@live.se>
2024-02-19 13:56:16 +0100

    Add react code to project

---
+++ .react-app/src/App.jsx
@@ -0,0 +1,44 @@
+import { useState } from "react"
+import "./styles.css"
+
+export default function App() {
+  const [newItem, setNewItem] = useState("")  // This is a React hook
+  const [todos, setTodos] = useState([])  // This is a React hook
+
+  function handleSubmit(e) {
+    e.preventDefault()
+
+    setTodos(currentTodos => {
+      return [
+        ...currentTodos,
+        { id: crypto.randomUUID(), title: newItem, completed: false },
+      ]
+    })
+  }
+  return (
+    <>
+      <form onSubmit={handleSubmit} className="new-item-form">
+        <div className="form-row">
+          <label htmlFor="item">New Item</label>
+          <input value={newItem}
+            onChange={e => setNewItem(e.target.value)} type="text" id="item" />
+        </div>
+        <button className="btn">Add</button>
+      </form>
+      <h1 className="header">To Do List:</h1>
+      <ul className="list">
+        {todos.map(todo => {
+          return <li key={todo.id}>
+            <label>
+              <input type="checkbox" checked={todo.completed} />
+              {todo.title}
+            </label>
+            <button className="btn btn-danger">Delete</button>
+          </li>
+        })}
+      </ul>
+    </>
+  )
+}
+
+
